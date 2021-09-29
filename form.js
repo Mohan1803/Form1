85b9179bbcbce1b72cfb8d41d0a18cbac31a5c44
Mohan1803 <89601675+Mohan1803@users.noreply.github.com>
2021-09-29 15:37:21 +0530

    first commit

---
+++ form.js
@@ -0,0 +1,47 @@
+function myFunc(){
+  const username = document.getElementById("1");
+  const email = document.getElementById("2");
+  const password = document.getElementById("3");
+  const password1 = document.getElementById("4"); 
+
+  //MAIL FORMAT
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+  //FOR PASSWORD FIELD
+
+  var lowerCaseLetters = /[a-z]/g;
+  var upperCaseLetters = /[A-Z]/g;
+  var numbers = /[0-9]/g;
+
+  
+ 
+
+
+
+
+
+   
+       if (username.value==0 || email.value==0 || password.value==0 || password1.value==0)
+       {
+        alert("PLEASE FILL THE DETAILS!!!");
+      } 
+      else if(!password.value.match(lowerCaseLetters) || !password.value.match(upperCaseLetters) ||
+      !password.value.match(numbers) || !password.value.length >= 8)
+      {  
+        document.getElementById('demo2').innerHTML="INSECURED PASSWORD!!!!";
+      }
+       else if(!email.value.match(mailformat))
+     { 
+       document.getElementById('demo1').innerHTML="You have entered an invalid email address!";
+     }
+       else if(password.value!=password1.value)
+     {
+       document.getElementById('demo').innerHTML="PASSWORD MISMATCH";
+     }
+       else
+     {
+        alert("SIGNUP SUCCESSFULL")
+     }
+     return false;
+}
