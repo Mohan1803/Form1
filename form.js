ea17772d9bdadae1dcfe09bd5b744a3ff527659a
Mohan1803 <89601675+Mohan1803@users.noreply.github.com>
2021-09-29 20:06:42 +0530

    say,file updated

--- form.js
+++ form.js
@@ -25,19 +25,32 @@ function myFunc(){
        if (username.value==0 || email.value==0 || password.value==0 || password1.value==0)
        {
         alert("PLEASE FILL THE DETAILS!!!");
-      } 
+      }
       else if(!password.value.match(lowerCaseLetters) || !password.value.match(upperCaseLetters) ||
       !password.value.match(numbers) || !password.value.length >= 8)
       {  
-        
-        document.getElementById('demo2').innerHTML="INSECURED PASSWORD!!!!";
+        document.getElementById('pwd').style.borderColor="#FF0000";
+        document.getElementById('demo2').innerHTML="Password Must has atleast 8 characters that include atleast 1 lowercase , 1 uppercase , 1 number & 1 special character in(!@#$%^&*)";
       }
-       else if(!email.value.match(mailformat))
+      else if(password.value.match(lowerCaseLetters) && password.value.match(upperCaseLetters) &&
+      password.value.match(numbers) && password.value.length >= 8){
+        document.getElementById('pwd').style.borderColor="green";
+        document.getElementById('demo2').innerHTML="";
+      }
+       
+     else if(!email.value.match(mailformat))
      { 
+      document.getElementById('email').style.borderColor="#FF0000";
       document.getElementById('demo1').innerHTML="You have entered an invalid email address!";
+     }
+     else if(email.value.match(mailformat))
+     { 
+      document.getElementById('email').style.borderColor="green";
+      document.getElementById('demo1').innerHTML="";
      }
        else if(password.value!=password1.value)
      {
+      document.getElementById('pwd1').style.borderColor="#FF0000";
        document.getElementById('demo').innerHTML="PASSWORD MISMATCH";
      }
        else
